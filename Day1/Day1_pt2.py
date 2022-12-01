144cca3ea3b30debdcb38cc5a8f3303637bc76b7
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2022-12-02 00:20:58 +0100

    Day 1: Calorie Counting

---
+++ Day1/Day1_pt2.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+fn = 'input.txt'
+tot = 0
+result = []
+p = Path(__file__).with_name(fn)
+
+with p.open('r') as fp:
+    for l in fp:
+        if l==' ' or l=='\n':
+            result.append(tot)
+            tot = 0
+        else:
+            tot += int(l) 
+    result.append(tot)
+
+result.sort(reverse=True)
+print(sum(result[0:3]))
