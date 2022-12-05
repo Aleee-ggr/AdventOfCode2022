4b7919e3d7c3e98fe62c88200be75ba7e8957b1a
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2022-12-06 00:13:02 +0100

    add Day 5: Supply Stacks

---
+++ Day5/Day5_pt1.py
@@ -0,0 +1,51 @@
+    #DAY5
+#PT1
+from pathlib import Path
+import re
+
+
+fn = 'input.txt'
+strings = []
+stacks = []
+rules = []
+result = []
+
+
+def Main():
+    stack = Input()
+    for rule in rules:
+        rule = list(rule[0])
+        rule = [int(p) for p in rule]
+        for i in range(0, rule[0]):
+            stack[rule[2]-1].append(stack[rule[1]-1].pop())
+    for i in range(0,9): 
+        stack[i].reverse()
+        result.append(stack[i][0].strip("[ ]"))
+    print(result)
+    
+
+def remove_values_from_list(the_list, val):
+   return [value for value in the_list if value != val]
+
+
+def Input():
+    p = Path(__file__).with_name(fn)
+    with p.open('r') as fp:
+        for l in fp:
+            strings.append(l.strip('\n'))
+    for i in range(0, 8):
+        stacks.append( [ strings[i][j:j+4] for j in range(0, len(strings[i]), 4) ] )
+    stack = list(zip(*stacks[::-1]))
+
+    for i in range(0,9):
+        stack[i] = remove_values_from_list(stack[i], '    ')
+        stack[i] = remove_values_from_list(stack[i], '   ')
+
+    for i, l in enumerate(strings):
+        if (i>9):
+            rules.append(re.findall('move (\d+) from (\d+) to (\d+)', l))
+    return stack
+
+
+if __name__ == '__main__':
+    Main()
